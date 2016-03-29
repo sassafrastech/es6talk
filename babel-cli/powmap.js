b93975c303ca2a419846426edd7f9c6bb43e73d4
Tom Smyth <tom@sassafras.coop>
2016-03-29 09:50:00 -0400

    Added Rails app

--- babel-cli/powmap.js
+++
@@ -1,5 +0,0 @@
-// Given an array of numbers and an optional exponent,
-// returns an array of the given numbers raised to the given exponent.
-function powMap(bases, exp = 2) {
-  return bases.map((x) => Math.pow(x, exp));
-}
