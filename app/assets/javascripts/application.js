9739145f6cfb68e05ffdb7c4c470e33734877f7b
Tom Smyth <tom@sassafras.coop>
2016-03-29 11:08:05 -0400

    Place selector step 1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,6 @@
-require('./main')
+//= require jquery
+//= require jquery_ujs
+
+var PlaceSelectorView = require('./place_selector_view')
+
+window.PlaceSelectorView = PlaceSelectorView.default
---
+++ app/assets/javascripts/place_selector_view.js
@@ -0,0 +1,8 @@
+class PlaceSelectorView {
+  constructor(options) {
+    this.options = options
+    console.log(options)
+  }
+}
+
+export default PlaceSelectorView
